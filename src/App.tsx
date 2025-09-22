6d3e279c74643a79072e58b984beacdacc411a2a
Diego Urbina <diegourbina.dev@gmail.com>
2025-09-21 19:28:30 -0500

    feature: se extendió título

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h1>Hello World !!!!!!!</h1>
+      <h4>By l4ner0</h4>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
